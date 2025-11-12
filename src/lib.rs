49b9a632a1d6da820c405ce80cd4512314ae8653
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-11-12 19:31:08 +0100

    fix: make RawImage public

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,15 @@
 //! Async stream of clipboard change events.
 //!
-//! Provides real-time clipboard monitoring through an async [`Stream`] interface.
+//! Provides real-time clipboard monitoring through an async [`Stream`](futures::Stream) interface.
 //!
 //! The main part of this crate is [`ClipboardStream`].
-//! This struct implements [`Stream`].
-//!
-//! # Example
-//! The following example shows how to receive clipboard items:
-//!
-//! ```no_run
-//! use clipboard_stream::{ClipboardEventListener, Body};
-//! use futures::stream::StreamExt;
-//!
-//! #[tokio::main]
-//! async fn main() {
-//!     // Spawn a clipboard event listener
-//!     let mut event_listener = ClipboardEventListener::spawn();
-//!
-//!     // Create a new stream
-//!     let mut stream = event_listener.new_stream(32);
-//!
-//!     while let Some(body) = stream.next().await {
-//!         if let Body::Utf8String(text) = body {
-//!             println!("{}", text);
-//!         }
-//!     }
-//! }
-//! ```
+//! This struct implements [`Stream`](futures::Stream).
 //!
 //! # Runtime
 //! Internally, this crate spawns a small dedicated OS thread to listen for clipboard events.
 //! The API itself is `Future`-based and does not depend on any specific async runtime,
 //! so it works with [`tokio`](https://docs.rs/tokio), [`smol`](https://docs.rs/smol), or any runtime compatible with
-//! [`futures`](https://docs.rs/futures).
-//!
-//! # Platforms
-//! - macOS
-//!
-//! Currently supported on **macOS only**. Windows support is planned for a future release.
-//!
-//! [`Stream`]: https://docs.rs/futures/latest/futures/stream/trait.Stream.html
-//! [`ClipboardStream`]: crate::stream::ClipboardStream
+//! [`futures`](::futures).
 mod body;
 mod driver;
 pub mod error;
@@ -57,4 +26,7 @@ mod win;
 
 pub use stream::{ClipboardStream, StreamId};
 
-pub use crate::{body::Body, event_listener::ClipboardEventListener};
+pub use crate::{
+  body::{Body, RawImage},
+  event_listener::ClipboardEventListener,
+};
